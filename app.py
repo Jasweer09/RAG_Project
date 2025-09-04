390fb7e9ce910c690c430e85a5cf26ba973c303d
Jasweer09 <jasweertadikonda@gmail.com>
2025-09-04 11:54:48 -0400

    Clean repo without secrets

---
+++ app.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import os
+from langchain_groq import ChatGroq
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.embeddings import OllamaEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+from langchain_community.vectorstores import FAISS
+from dotenv import load_dotenv
+import time
+load_dotenv()
+
+##Load the Grok api
+groq_api_key = os.environ["GROQ_API_KEY"]
+
+if "vector" not in st.session_state:
+    st.session_state.embeddings = OllamaEmbeddings()
+    st.session_state.loader = WebBaseLoader("https://docs.langchain.com/langsmith/observability-quickstart")
+    st.session_state.docs=st.session_state.loader.load()
+    st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 200)
+    st.session_state.final_documents = st.session_state.text_splitter.split_documents(st.session_state.docs[:100])
+    st.session_state.vectors = FAISS.from_documents(st.session_state.final_documents, st.session_state.embeddings)
+st.title("ChatGroq")
+
+llm = ChatGroq(groq_api_key = groq_api_key,
+               model="gemma2-9b-it")
+
+prompt = ChatPromptTemplate.from_template(
+    """
+    Answer the questions based on the provided context only.
+    Please provide the most accurate response based on the question
+    <context>
+        {context}
+    <context>
+    Question: {input}
+    """
+)
+document_chain = create_stuff_documents_chain(llm, prompt)
+retriever = st.session_state.vectors.as_retriever()
+retrival_chain = create_retrieval_chain(retriever, document_chain)
+
+prompt = st.text_input("input you prompt here")
+
+if prompt:
+    start = time.process_time()
+    response = retrival_chain.invoke({"input": prompt})
+    print("Response time: ", time.process_time()-start)
+    st.write(response['answer'])
+
+    with st.expander("Document Similarity Search"):
+        for i, doc in enumerate(response["context"]):
+            st.write(doc.page_content)
+            st.write("-----------------------------------")
